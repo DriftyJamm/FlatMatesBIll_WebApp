2538d6539ca77cbbd3f57f799531162b0ce9a80d
DriftyJamm <92976385+DriftyJamm@users.noreply.github.com>
2025-04-17 20:38:46 +0530

    updated methods

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 from flask.views import MethodView
-from wtforms import Form
-from flask import Flask
+from wtforms import Form, StringField, SubmitField
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 class HomePage(MethodView):
     def get(self):
-        return "Hello"
+        return render_template('index.html')
 
 class BillFormPage(MethodView):
     def get(self):
-        return "I am a bill from this page"
+        bill_form = BillForm()
+        return render_template('bill_form_page.html', billform=bill_form)
 
 class ResultsPage(MethodView):
     pass
 
 class BillForm(Form):
-    pass
+    amount = StringField("Bill Amount: ")
+    period = StringField("Bill Period: ")
+
+    name1 = StringField("Name: ")
+    days_in_house1 = StringField("Days in house: ")
+
+    name2 = StringField("Name: ")
+    days_in_house2 = StringField("Days in house: ")
+
+    button = SubmitField("Calculate")
 
 app.add_url_rule('/', view_func = HomePage.as_view('home_page'))
-app.add_url_rule('/bill', view_func = BillFormPage.as_view('bill_form_page'))
+app.add_url_rule('/bill_form', view_func = BillFormPage.as_view('bill_form_page'))
 
 app.run()
